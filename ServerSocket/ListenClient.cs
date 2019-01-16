6aa3207593603fcded00111e0e3d8ae7032194f0
BrianLan <brian7lan3@gmail.com>
2019-01-16 15:48:24 +0800

    建立 ServerSocket.cs 和 ListenClient.cs 完成

--- ServerSocket/ListenClient.cs
+++ ServerSocket/ListenClient.cs
@@ -16,5 +16,31 @@ namespace ServerSocket
         private System.Net.Sockets.Socket clientSocket;
         
         // 建構函式
+        public ListenClient(Socket serverSocket)
+        {
+            this.serverSocket = serverSocket;
+        }
+
+        public void ServerThreadProc()
+        {
+            while (true)
+            {
+                try
+                {
+                    // 處理用戶端連線
+                    clientSocket = serverSocket.Accept();
+
+                    // 取得本機相關的網路資訊
+                    IPEndPoint serverInfo = (IPEndPoint)clientSocket.LocalEndPoint;
+
+                    // 取得連結用戶端相關的網路連接資訊
+                    IPEndPoint clientInfo = (IPEndPoint)clientSocket.RemoteEndPoint;
+
+                    Console.WriteLine("Server: " + serverInfo.Address.ToString() + ":" + serverInfo.Port.ToString());
+                    Console.WriteLine("Client: " + clientInfo.Address.ToString() + ":" + clientInfo.Port.ToString());
+                }
+                catch (Exception ex){ }
+            }
+        }
     }
 }
